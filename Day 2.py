31f6bdb727a68eec0be3f4b5aacce1d6846223a1
Jyoti Prasad <prasadjyoti836@gmail.com>
2023-08-29 23:55:17 +0530

    day 2 coding completed

---
+++ Day 2.py
@@ -0,0 +1,27 @@
+#Python | Count occurrences of an element in a list
+
+def count_value(list_of_num, x):
+    count_of_x = 0
+    for element in list_of_num:
+        if element == x:
+            count_of_x += 1
+    return count_of_x
+
+
+print(count_value([15, 6, 7, 10, 12, 20, 10, 28, 10], 10))
+
+
+#Break a list into chunks of size N in Python
+
+def divide_chunks(list_1, n):
+    new_list = []
+    x = 0
+    for i in range(x, x + n):
+        new_list.append(list_1[x: x + n])
+        x = x + n
+    return new_list
+
+
+print(divide_chunks(['geeks', 'for', 'geeks', 'like','geeky','nerdy', 'geek', 'love','questions','words', 'life'], 4))
+
+
