b24bd9edcdd38d08d8dffb5bed7ea85f6281f93f
Jyoti Prasad <prasadjyoti836@gmail.com>
2025-01-27 22:54:58 +0530

    longest substring question

--- Day_24.py
+++ Day_24.py
@@ -8,3 +8,33 @@ def reverse_string(string_1):
 a = 'i.like.this.program.very.much' 
 
 print(reverse_string(a))
+
+
+# Longest Common Prefix using Sorting
+
+
+
+def longest_string(arr):
+    arr.sort()
+
+    first_word = arr[0]
+    last_word = arr[-1]
+    print(first_word, last_word)
+    i = 0
+    min_length = len(first_word)
+    longest_str = ""
+    while i < min_length and first_word[i] == last_word[i]:
+        i = i + 1
+
+
+    return first_word[:i]
+
+
+
+
+arr = ["apple", "ape", "april", "apricot"]
+#arr= ["hello", "world"]
+print(longest_string(arr))
+
+
+       
